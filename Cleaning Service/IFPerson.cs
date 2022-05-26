fdbd3c0eb6a23430f5086ff572a403c6460d8d74
EGGsMuffin <MUL19002132@btc.ac.uk>
2022-05-26 08:57:18 +0100

    Added interface for person and staff

---
+++ Cleaning Service/IFPerson.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cleaning_Service
+{
+    internal interface IFPerson
+    {
+        public Guid Id { get; set; }
+        public string Fname { get; set; }
+        public string Sname { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ Cleaning Service/IFStaff.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Cleaning_Service.Enums;
+
+namespace Cleaning_Service
+{
+    internal interface IFStaff
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public StaffType Type { get; set; }
+    }
+}
